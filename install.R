b4f80e14aa25a8c3ddfa4bb15245376ef7232a55
Tom Bishop Local <l_trpb2@mica-dev.mrc-epid.cam.ac.uk>
2021-08-02 13:56:17 +0100

    changed branch for dsSurvival

--- install.R
+++ install.R
@@ -5,6 +5,7 @@ library(opalr)
 o <- opal.login(username = "administrator", password = "datashieldtest", url = "http://localhost:9980")
 oadmin.install_devtools(o)
 dsadmin.install_github_package(o, pkg = "dsSynthetic", username = "tombisho", ref = "snapshot", profile = "default")
-dsadmin.install_github_package(o, pkg = "dsSurvival", username = "neelsoumya", ref = "v1.0.0", profile = "default")
+#dsadmin.install_github_package(o, pkg = "dsSurvival", username = "neelsoumya", ref = "v1.0.0", profile = "default")
+dsadmin.install_github_package(o, pkg = "dsSurvival", username = "neelsoumya", ref = "main", profile = "default")
 
 opal.logout(o)
