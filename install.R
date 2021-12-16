db12f4428b2f10222e724cbe1b25d406e409b8b8
Tom Bishop Local <l_trpb2@mica-dev.mrc-epid.cam.ac.uk>
2021-12-16 20:20:30 +0000

    new opal requires https and to disable certificate checking

--- install.R
+++ install.R
@@ -2,7 +2,10 @@ if (!require(opalr)) {
   install.packages("opalr", repos = "https://cloud.r-project.org", dependencies = TRUE)
 }
 library(opalr)
-o <- opal.login(username = "administrator", password = "datashieldtest", url = "http://localhost:9980")
+options(opal.opts = list(
+           ssl_verifyhost = 0,
+           ssl_verifypeer = 0))
+o <- opal.login(username = "administrator", password = "datashieldtest", url = "https://localhost:9943")
 oadmin.install_devtools(o)
 dsadmin.install_github_package(o, pkg = "dsSynthetic", username = "tombisho", ref = "snapshot", profile = "default")
 #dsadmin.install_github_package(o, pkg = "dsSurvival", username = "neelsoumya", ref = "v1.0.0", profile = "default")
